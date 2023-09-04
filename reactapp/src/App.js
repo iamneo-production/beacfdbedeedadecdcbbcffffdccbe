6519fb1e8ffeb37dc5cc729d1225feebb066f7fb
Iamneo <techadmin@iamneo.ai>
2023-09-04 15:19:08 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,30 +1,24 @@
-import React from 'react';
-import Auth from './components/Auth/Auth';
-import Register from './components/Auth/Register/Register';
-import Login from './components/Auth/Login/Login';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import Homepage from './components/Customer/Homepage';
-import AdminHomePage from './components/Admin/AdminHomePage';
-import AddCenter from './components/Admin/AddCenter';
-import Dashboard from './components/Customer/Dashboard';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Auth />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/user/homepage" element={<Homepage />} />
-          <Route path="/admin" element={<AdminHomePage />} />
-          <Route path="/admin/addServiceCenter" element={<AddCenter />} />
-          <Route path="/user/dashboard/" element={<Dashboard />} />
-        </Routes>
-      </div>
-    </Router>
-  
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
