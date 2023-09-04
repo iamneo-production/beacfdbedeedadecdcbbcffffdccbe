af1a21cf7066dc59c96c230bc3aee42f8a150528
Iamneo <techadmin@iamneo.ai>
2023-09-04 09:56:21 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import Auth from './components/Auth/Auth'; 
+import Auth from './components/Auth/Auth';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Auth />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Auth />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
+  
   );
 }
 
--- reactapp/src/components/Auth/Auth.js
+++ reactapp/src/components/Auth/Auth.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import './Auth.css';
+import { useNavigate } from 'react-router-dom';
 
 const LandingPage = () => {
+  const navigate = useNavigate();
+  function nav(){
+    navigate("/");
+  }
   return (
     <div className="landing-page">
       <h1>Welcome to vacServ</h1>
       <p>Your Vacuum Cleaner Service Provider</p>
       <div className="buttons-container">
         {/* <Link to="/login" className="nav-link"> */}
-          <button className="login-button" onClick ={()=>nav()) >Login</button>
-        {/* </Link> */}
+          <button className="login-button" onClick ={()=>nav()}>Login</button>
+
+
         {/* <Link to="/register" className="nav-link"> */}
           <button className="register-button">Register</button>
         {/* </Link> */}
       </div>
-      </div>
+      
     </div>
   );
 };
