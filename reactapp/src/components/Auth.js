adfc916ced827bb684eb7d3e4738b7d3085b945c
Iamneo <techadmin@iamneo.ai>
2023-09-04 08:26:23 +0000

    Backup Commit

---
+++ reactapp/src/components/Auth.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Auth() {
+  return (
+    <div>
+      <h2>Authentication Component</h2>
+      <p>This is a simple authentication component.</p>
+    </div>
+  );
+}
+
+export default Auth;
