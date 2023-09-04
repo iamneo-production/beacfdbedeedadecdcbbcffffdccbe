8156bcf1c7c57062398a57ca9803ee09ca6f78d6
Iamneo <techadmin@iamneo.ai>
2023-09-04 10:01:24 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Auth from './components/Auth/Auth';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Auth/Login/Login';
 
 function App() {
   return (
