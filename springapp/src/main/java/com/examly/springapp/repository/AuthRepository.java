e9bbf399186631554883a38a8faf996227aa09fe
Iamneo <techadmin@iamneo.ai>
2023-09-09 06:08:12 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/repository/AuthRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/AuthRepository.java
@@ -1,10 +1,10 @@
-package com.examly.springapp.repository;
-
-import com.examly.springapp.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.examly.springapp.model.UserModel;
 
 @Repository
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-    // You can add custom query methods here if needed
+public interface AuthRepository extends JpaRepository<UserModel, Long> {
+
+    // Custom query method to find a user by email and user role
+    UserModel findByEmailAndUserRole(String email, String userRole);
 }
--- springapp/src/main/java/com/examly/springapp/service/AuthService.java
+++ springapp/src/main/java/com/examly/springapp/service/AuthService.java
@@ -1,3 +1,48 @@
-public class ProductService {
-    
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.examly.springapp.model.LoginModel;
+import com.examly.springapp.model.UserModel;
+import com.examly.springapp.repository.AuthRepository;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private AuthRepository authRepository;
+
+    public boolean isAdminPresent(LoginModel loginData) {
+        // Check if an admin with the provided email exists in the database
+        UserModel admin = authRepository.findByEmailAndUserRole(loginData.getEmail(), "admin");
+        
+        // If an admin is found and the password matches, return true
+        return admin != null && admin.getPassword().equals(loginData.getPassword());
+    }
+
+    public String saveAdmin(UserModel userData) {
+        // Set the user role to "admin" before saving to the database
+        userData.setUserRole("admin");
+        
+        // Save the admin to the database
+        authRepository.save(userData);
+        
+        return "Admin added successfully";
+    }
+
+    public boolean isUserPresent(LoginModel loginData) {
+        // Check if a user with the provided email exists in the database
+        UserModel user = authRepository.findByEmailAndUserRole(loginData.getEmail(), "user");
+        
+        // If a user is found and the password matches, return true
+        return user != null && user.getPassword().equals(loginData.getPassword());
+    }
+
+    public String saveUser(UserModel userData) {
+        // Set the user role to "user" before saving to the database
+        userData.setUserRole("user");
+        
+        // Save the user to the database
+        authRepository.save(userData);
+        
+        return "User added successfully";
+    }
 }
