cf8eec3645bee43106aae12b4a4e1e433e079a59
Iamneo <techadmin@iamneo.ai>
2023-09-14 18:58:28 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/controller/ServiceCenterController.java
+++ springapp/src/main/java/com/examly/springapp/controller/ServiceCenterController.java
@@ -39,8 +39,8 @@ public class ServiceCenterController {
         return ResponseEntity.notFound().build();
     }
     // Update the serviceCenter object with the new data
-    serviceCenter.setName(updatedServiceCenter.getName());
-    serviceCenter.setAddress(updatedServiceCenter.getAddress());
+    serviceCenter.setServiceCenterName(updatedServiceCenter.getName());
+    serviceCenter.setServiceCenterAddress(updatedServiceCenter.getAddress());
     serviceCenter
     // Update other fields as needed
     
