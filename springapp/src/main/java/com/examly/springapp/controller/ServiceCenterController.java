2071b66bd85462cf8e14a2a9d2b2bfc67e92da89
Iamneo <techadmin@iamneo.ai>
2023-09-14 18:18:29 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/controller/ServiceCenterController.java
+++ springapp/src/main/java/com/examly/springapp/controller/ServiceCenterController.java
@@ -3,11 +3,10 @@ package com.examly.springapp.controller;
 import com.examly.springapp.model.ServiceCenter;
 import com.examly.springapp.service.ServiceCenterService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+import javax.persistence.*;
 
 
 @RestController
