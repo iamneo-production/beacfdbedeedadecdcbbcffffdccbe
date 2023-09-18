a6722c2ce5a190aebfdd22fdeacc13ce1b05109a
Iamneo <techadmin@iamneo.ai>
2023-09-18 17:32:26 +0000

    Backup Commit

--- reactapp/src/components/Admin/GridCardsAdmin.js
+++ reactapp/src/components/Admin/GridCardsAdmin.js
@@ -58,7 +58,6 @@ export default function GridCardsAdmin({ searchTerm, sortOrder, serviceCenters }
       .catch((error) => {
         console.error("Error deleting service center:", error);
       });
-      window.location.reload();
   };
 
   // Filter the cards based on the search term
