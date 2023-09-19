5aa71317ad35bed5ceaf9186e04c57d34fe3c3f2
Iamneo <techadmin@iamneo.ai>
2023-09-19 06:21:58 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/controller/AppointmentController.java
+++ springapp/src/main/java/com/examly/springapp/controller/AppointmentController.java
@@ -2,9 +2,26 @@ package com.examly.springapp.controller;
 
 import com.examly.springapp.model.Product;
 import com.examly.springapp.model.User;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import javax.persistence.*;
+import java.util.*;
 
 
 @RestController
 @RequestMapping("/user")
 @CrossOrigin("*")
+public class AppointmentController{
+    @Autowired
+    private ProductService productService;
+    @Autowired
+    private ServiceCenterService serviceCenterService;
+
+
+    @GetMapping("/appointments/user/{userId}")
+    public ResponseEntity<List<Product>> getAppointmentsByUserId(@PathVariable Long userId) {
+    List<Product> userAppointments = productService.getAppointmentsByUserId(userId);
+    return ResponseEntity.ok(userAppointments);
+}
+}
