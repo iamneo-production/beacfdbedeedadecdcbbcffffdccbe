6c6045383417588c00ff74bc2db691e013f449f2
Iamneo <techadmin@iamneo.ai>
2023-09-19 06:36:58 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/repository/ProductRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/ProductRepository.java
@@ -2,6 +2,9 @@ package com.examly.springapp.repository;
 
 import com.examly.springapp.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
