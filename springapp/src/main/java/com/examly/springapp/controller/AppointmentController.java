958faee4c60c5bad31a891cfb11640679f4bbc33
Iamneo <techadmin@iamneo.ai>
2023-09-19 06:16:58 +0000

    Backup Commit

---
+++ springapp/src/main/java/com/examly/springapp/controller/AppointmentController.java
@@ -0,0 +1,10 @@
+package com.examly.springapp.controller;
+
+import com.examly.springapp.model.Product;
+import com.examly.springapp.model.User;
+
+
+
+@RestController
+@RequestMapping("/user")
+@CrossOrigin("*")
