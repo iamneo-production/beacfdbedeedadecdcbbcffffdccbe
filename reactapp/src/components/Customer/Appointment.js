8c17eae23d7ae4a7e4dbb3713362a1aba4f84afb
Iamneo <techadmin@iamneo.ai>
2023-09-20 09:01:34 +0000

    Backup Commit

--- reactapp/src/components/Customer/Appointment.js
+++ reactapp/src/components/Customer/Appointment.js
@@ -231,7 +231,7 @@ export default function Appointment() {
         productId={editProductId} 
         setUserAppointments={setUserAppointments}
         updateAppointments={updateAppointments}
-        serviceCenterName={appointment.serviceCenterName}
+        serviceCenterName={serviceCenterName}
         />
       <ReviewModal reviewOpen={reviewOpen} handleClose={handleReviewClose} />
     </div>
