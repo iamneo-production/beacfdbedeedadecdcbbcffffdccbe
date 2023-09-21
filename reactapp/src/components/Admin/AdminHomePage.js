3c9513a21c73573a201a897a37ecd6069cd6b80d
Iamneo <techadmin@iamneo.ai>
2023-09-21 06:32:47 +0000

    Backup Commit

--- reactapp/src/components/Admin/AdminHomePage.js
+++ reactapp/src/components/Admin/AdminHomePage.js
@@ -41,6 +41,77 @@ const styles = {
   },
 };
 
+function AdminHomePage() {
+  const params = useParams();
+  const { userId } = params;
 
+  const [numberOfCustomers, setNumberOfCustomers] = useState(0);
+  const [numberOfServices, setNumberOfServices] = useState(0);
+  const [numberOfAppointments, setNumberOfAppointments] = useState(0);
+
+  useEffect(() => {
+    // Fetch data when the component mounts
+    fetch(`https://8080-beacfdbedeedadecdcbbcffffdccbe.premiumproject.examly.io/admin/total-customers`)
+      .then((response) => response.json())
+      .then((data) => setNumberOfCustomers(data.numberOfCustomers));
+
+    fetch(`https://8080-beacfdbedeedadecdcbbcffffdccbe.premiumproject.examly.io/admin/total-service-centers`)
+      .then((response) => response.json())
+      .then((data) => setNumberOfServices(data.numberOfServices));
+
+    fetch(`https://8080-beacfdbedeedadecdcbbcffffdccbe.premiumproject.examly.io/admin/total-products`)
+      .then((response) => response.json())
+      .then((data) => setNumberOfAppointments(data.numberOfAppointments));
+  }, []);
+
+  return (
+    <div>
+      <AdminNavbar />
+      <Container maxWidth="lg" style={styles.container}>
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          color="text.primary"
+          gutterBottom
+          style={{ marginTop: '5%' }}
+        >
+          Welcome to the Admin Dashboard
+        </Typography>
+        {numberOfCustomers !== 0 && numberOfServices !== 0 && numberOfAppointments !== 0 ? (
+          <div style={styles.boxContainer}>
+            <Paper elevation={3} style={styles.box}>
+              <Typography variant="h6">
+                No. of proud customers we've had
+              </Typography>
+              <Typography variant="h4" style={styles.number}>
+                {console.log("number of numberOfCustomer: ", numberOfCustomers)}
+                {numberOfCustomers}
+              </Typography>
+            </Paper>
+            <Paper elevation={3} style={styles.box}>
+              <Typography variant="h6">No. of services we have</Typography>
+              <Typography variant="h4" style={styles.number}>
+                {numberOfServices}
+              </Typography>
+            </Paper>
+            <Paper elevation={3} style={styles.box}>
+              <Typography variant="h6">No. of Sucessful Appointments</Typography>
+              <Typography variant="h4" style={styles.number}>
+                {numberOfAppointments}
+              </Typography>
+            </Paper>
+          </div>
+        ) : (
+          // Optionally, you can show a loading message or spinner here while data is being fetched
+          <Typography variant="h6" align="center">
+            Loading...
+          </Typography>
+        )}
+      </Container>
+      <Footer />
+    </div>
+  );
+}
 
 export default AdminHomePage;
