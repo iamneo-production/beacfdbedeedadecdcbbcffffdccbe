fabb6e0847747927c85ae41c7e9693bc937fa3f9
Iamneo <techadmin@iamneo.ai>
2023-09-22 13:10:43 +0000

    Backup Commit

--- reactapp/src/components/Auth/SignUp/SignUp.js
+++ reactapp/src/components/Auth/SignUp/SignUp.js
@@ -102,6 +102,7 @@ export default function Register() {
 
     // Basic Validation Rules
     let isValid = true;
+    const userRoleLower = formData.userRole.toLowerCase();
     if (formData.userRole.trim() === "") {
       setErrors((prevErrors) => ({
         ...prevErrors,
